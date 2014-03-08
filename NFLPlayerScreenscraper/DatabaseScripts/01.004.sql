c9aa1dbcbe5d13d75476b75ca3d5dcb2148eba89
TKirby42 <jimgorman2004@yahoo.com>
2014-03-08 00:23:03 -0500

    Add creation scripts for the Comments and Users tables.

---
+++ NFLPlayerScreenscraper/DatabaseScripts/01.004.sql
@@ -0,0 +1,92 @@
+﻿USE [NFL_Database]
+GO
+IF  EXISTS (SELECT * FROM sys.default_constraints WHERE object_id = OBJECT_ID(N'[dbo].[DF_Comments_Deleted]') AND parent_object_id = OBJECT_ID(N'[dbo].[Comments]'))
+Begin
+IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[DF_Comments_Deleted]') AND type = 'D')
+BEGIN
+ALTER TABLE [dbo].[Comments] DROP CONSTRAINT [DF_Comments_Deleted]
+END
+
+
+End
+GO
+/****** Object:  ForeignKey [FK_Comments_Players]    Script Date: 03/08/2014 00:17:47 ******/
+IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Comments_Players]') AND parent_object_id = OBJECT_ID(N'[dbo].[Comments]'))
+ALTER TABLE [dbo].[Comments] DROP CONSTRAINT [FK_Comments_Players]
+GO
+/****** Object:  ForeignKey [FK_Comments_Users]    Script Date: 03/08/2014 00:17:47 ******/
+IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Comments_Users]') AND parent_object_id = OBJECT_ID(N'[dbo].[Comments]'))
+ALTER TABLE [dbo].[Comments] DROP CONSTRAINT [FK_Comments_Users]
+GO
+/****** Object:  Table [dbo].[Comments]    Script Date: 03/08/2014 00:17:47 ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Comments]') AND type in (N'U'))
+DROP TABLE [dbo].[Comments]
+GO
+/****** Object:  Table [dbo].[Users]    Script Date: 03/08/2014 00:17:47 ******/
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Users]') AND type in (N'U'))
+DROP TABLE [dbo].[Users]
+GO
+/****** Object:  Table [dbo].[Users]    Script Date: 03/08/2014 00:17:47 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Users]') AND type in (N'U'))
+BEGIN
+CREATE TABLE [dbo].[Users](
+	[GoogleID] [nvarchar](50) NOT NULL,
+	[DisplayName] [nvarchar](100) NOT NULL,
+ CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED 
+(
+	[GoogleID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+END
+GO
+/****** Object:  Table [dbo].[Comments]    Script Date: 03/08/2014 00:17:47 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Comments]') AND type in (N'U'))
+BEGIN
+CREATE TABLE [dbo].[Comments](
+	[CommentID] [int] IDENTITY(1,1) NOT NULL,
+	[PlayerID] [int] NOT NULL,
+	[GoogleID] [nvarchar](50) NOT NULL,
+	[Comment] [nvarchar](140) NOT NULL,
+	[Deleted] [bit] NOT NULL,
+ CONSTRAINT [PK_Comments] PRIMARY KEY CLUSTERED 
+(
+	[CommentID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+END
+GO
+/****** Object:  Default [DF_Comments_Deleted]    Script Date: 03/08/2014 00:17:47 ******/
+IF Not EXISTS (SELECT * FROM sys.default_constraints WHERE object_id = OBJECT_ID(N'[dbo].[DF_Comments_Deleted]') AND parent_object_id = OBJECT_ID(N'[dbo].[Comments]'))
+Begin
+IF NOT EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[DF_Comments_Deleted]') AND type = 'D')
+BEGIN
+ALTER TABLE [dbo].[Comments] ADD  CONSTRAINT [DF_Comments_Deleted]  DEFAULT ((0)) FOR [Deleted]
+END
+
+
+End
+GO
+/****** Object:  ForeignKey [FK_Comments_Players]    Script Date: 03/08/2014 00:17:47 ******/
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Comments_Players]') AND parent_object_id = OBJECT_ID(N'[dbo].[Comments]'))
+ALTER TABLE [dbo].[Comments]  WITH CHECK ADD  CONSTRAINT [FK_Comments_Players] FOREIGN KEY([PlayerID])
+REFERENCES [dbo].[Players] ([PlayerID])
+GO
+IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Comments_Players]') AND parent_object_id = OBJECT_ID(N'[dbo].[Comments]'))
+ALTER TABLE [dbo].[Comments] CHECK CONSTRAINT [FK_Comments_Players]
+GO
+/****** Object:  ForeignKey [FK_Comments_Users]    Script Date: 03/08/2014 00:17:47 ******/
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Comments_Users]') AND parent_object_id = OBJECT_ID(N'[dbo].[Comments]'))
+ALTER TABLE [dbo].[Comments]  WITH CHECK ADD  CONSTRAINT [FK_Comments_Users] FOREIGN KEY([GoogleID])
+REFERENCES [dbo].[Users] ([GoogleID])
+GO
+IF  EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_Comments_Users]') AND parent_object_id = OBJECT_ID(N'[dbo].[Comments]'))
+ALTER TABLE [dbo].[Comments] CHECK CONSTRAINT [FK_Comments_Users]
+GO
